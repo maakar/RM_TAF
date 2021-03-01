c97868ef05229c622a1c277a5faa5fae39d46ffb
kitMakar <makar.copy@gmail.com>
2021-03-01 23:34:20 +0200

    Added first test

---
+++ src/test/java/org/taf/tests/BaseTest.java
@@ -0,0 +1,24 @@
+package org.taf.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.junit.ScreenShooter;
+import com.codeborne.selenide.junit.TextReport;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+
+public abstract class BaseTest {
+
+
+    @Rule
+    public TestRule report = new TextReport().onFailedTest(true).onSucceededTest(true);
+    @Rule
+    public ScreenShooter screenShooter = ScreenShooter.failedTests();
+
+    @Before
+    public void confidurationSetup() {
+        Configuration.baseUrl = "http://automationpractice.com";
+    }
+
+
+}
