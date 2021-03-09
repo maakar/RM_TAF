28fe20469030b3a2b57514c9272cb3acdee1ea1d
kitMakar <makar.copy@gmail.com>
2021-03-09 18:47:11 +0200

    AbstractPageWithHeader added

--- src/test/java/org/taf/pages/AuthentificationPage.java
+++ src/test/java/org/taf/pages/AuthentificationPage.java
@@ -2,7 +2,7 @@ package org.taf.pages;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 
-public class AuthentificationPage extends AbstractBasePage {
+public class AuthentificationPage extends AbstractPageWithHeader {
 
     private static final String DEFAULT_URL = "/index.php?controller=authentication&back=my-account";
 
--- src/test/java/org/taf/pages/HomePage.java
+++ src/test/java/org/taf/pages/HomePage.java
@@ -5,28 +5,10 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.$;
 
-public class HomePage extends AbstractBasePage {
+public class HomePage extends AbstractPageWithHeader {
 
 
     private static final String DEFAULT_URL = "/index.php";
-
-    private final SelenideElement banner = $("#header .banner");
-
-    private final SelenideElement navBar = $(".nav .row");
-
-    private final SelenideElement contactLinkNavBar = $("#contact-link");
-
-    private final SelenideElement signInButton = $(".header_user_info");
-
-    private final SelenideElement headerLogo = $("#header_logo");
-
-    private final SelenideElement searchBoxTop = $("#searchbox");
-
-    private final SelenideElement shoppingCartButton = $(".shopping_cart > a:nth-child(1)");
-
-    private final SelenideElement topBlockMenu = $("#block_top_menu>ul");
-
-    private final SelenideElement topBlockMenuButtons = $("#block_top_menu > ul > li");
 
     private final SelenideElement sliderRow = $("#slider_row");
 
